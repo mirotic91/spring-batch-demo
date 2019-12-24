b088b933b7b0df425d912e6c7649ad2f1a33a41b
Jayden <jonguk1221@gmail.com>
2019-12-24 15:22:46 +0900

    simple config

---
+++ src/main/java/me/mirotic/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package me.mirotic.demo;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
